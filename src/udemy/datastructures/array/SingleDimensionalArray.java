2068f8a7d70e3bf9de78b6419905ca7df3162080
miidlaj <mumidlaj@gmail.com>
2023-05-20 12:17:12 +0530

    array

--- src/udemy/datastructures/array/SingleDimensionalArray.java
+++ src/udemy/datastructures/array/SingleDimensionalArray.java
@@ -1,11 +1,18 @@
 package udemy.datastructures.array;
 
+import java.util.Arrays;
+
 public class SingleDimensionalArray {
 
     int arr[] = null;
 
     public SingleDimensionalArray(int size) {
         arr = new int[size];
+
+        /**
+         * By default, the array values will be 0;
+         * here we set it as min integer value of java for identifying and checking already inserted.
+         */
         for (int i = 0; i < arr.length; i++) {
             arr[i] = Integer.MIN_VALUE;
         }
@@ -24,6 +31,10 @@ public class SingleDimensionalArray {
 
     }
 
+    public void display() {
+        System.out.println(Arrays.toString(arr));
+    }
+
     public static void main(String[] args) {
 
         SingleDimensionalArray array = new SingleDimensionalArray(10);
@@ -31,5 +42,7 @@ public class SingleDimensionalArray {
         array.insert(9,5);
         array.insert(2,1);
         array.insert(9,10);
+        array.display();
+
     }
 }
