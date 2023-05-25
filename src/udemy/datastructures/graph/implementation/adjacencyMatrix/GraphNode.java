c15439e0ac514b43af5105a662980b0f7fc1ab72
miidlaj <mumidlaj@gmail.com>
2023-05-25 11:47:06 +0530

    Graph Implementation using matrix and list

---
+++ src/udemy/datastructures/graph/implementation/adjacencyMatrix/GraphNode.java
@@ -0,0 +1,13 @@
+package udemy.datastructures.graph.implementation.adjacencyMatrix;
+
+public class GraphNode {
+
+    public String name;
+    public int index;
+
+    public GraphNode(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+
+}
