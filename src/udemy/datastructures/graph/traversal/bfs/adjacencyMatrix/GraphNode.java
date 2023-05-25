a2964a8d2d98711479d21e1f3bd81f3b604e296d
miidlaj <mumidlaj@gmail.com>
2023-05-25 16:09:35 +0530

    BFS for matrix

---
+++ src/udemy/datastructures/graph/traversal/bfs/adjacencyMatrix/GraphNode.java
@@ -0,0 +1,14 @@
+package udemy.datastructures.graph.traversal.bfs.adjacencyMatrix;
+
+public class GraphNode {
+
+    public String name;
+    public int index;
+    public boolean isVisited = false;
+
+    public GraphNode(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+
+}
